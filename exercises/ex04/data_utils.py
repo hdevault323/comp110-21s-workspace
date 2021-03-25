63499bd4649e576a637d6ddbfca28eb594ea2593
Hannah Devault <hdevault@live.unc.edu>
2021-03-25 19:16:34 -0400

    Exercise 04 progress

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -1,18 +1,30 @@
 """Utility functions for wrangling data."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730236759"
 
 
 from csv import DictReader
 
-
 def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
+    file_handle = open(csv_file, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
     rows: list[dict[str, str]] = []
-    
-    # TODO 0.1) Complete the implementation of this function here.
+    for row in csv_reader:
+        rows.append(row)
+    return rows
+    file_handle.close()
+
+
+def column_values(data_rows: list[dict[str, str]], column_name: str) -> list[str]:
+    values_of_columns: list[str] = []
+    for row in data_rows
+        value_of_columns.append(str(row["subject_age]))
+    return values_of_columns
+
 
     return rows
 
 
 # TODO: Define the other functions here.
+
